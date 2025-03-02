071bf73d747d0c5daae9d6071808a09fc244df7f
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-02 13:19:31 +0600

    condeton

---
+++ module-18/multilavelCondetion.js
@@ -0,0 +1,8 @@
+const price =50000;
+if(price >=50000){
+    // 10 pursent 
+    const tenp= price*10 /100;
+    console.log(tenp);
+    const pament = price - tenp;
+    console.log(pament);
+}
---
+++ module-18/test.js
@@ -0,0 +1,3 @@
+const salare =15000;
+if(salare > 10000) console.log("good job");
+else console.log("no job");
