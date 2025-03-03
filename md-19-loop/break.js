6aaa9f32e30f9dd011bd6be172f2a81743ed709d
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-03 22:07:02 +0600

    break

---
+++ md-19-loop/break.js
@@ -0,0 +1,8 @@
+for(let a=1; a<=100; a++){
+    // console.log(a);
+    if(a > 5){
+        break;
+
+    }
+    console.log(a);
+}
--- md-19-loop/multi.js
+++ md-19-loop/multi.js
@@ -1,5 +1,15 @@
-for(let i=1; i<=30; i++){
-    if(i%3 == 0 || i%5 == 0){
-        console.log(i);
+// for(let i=1; i<=30; i++){
+//     if(i%3 == 0 || i%5 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+let a = 0;
+for(let i=1; i<=50; i++){
+    if(i%7 === 0){
+       a+=i;
+
     }
 }
+console.log(a);
