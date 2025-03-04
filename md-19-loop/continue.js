5721701a44f55bf760bd3d93b3372ef81acd721c
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-04 10:24:31 +0600

    commit

---
+++ md-19-loop/continue.js
@@ -0,0 +1,12 @@
+let n = 0;
+while(n<=50){
+    console.log("age 0 print-->",n);
+    n++;
+
+    if( n % 5 !==0){
+        console.log("continue  ",n);
+        continue;
+    }
+    console.log(n);
+    
+}
