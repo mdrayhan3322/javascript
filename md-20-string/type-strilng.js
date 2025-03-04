18b801fc35252438fa32e52447f0fe4b1016b609
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-04 11:46:28 +0600

    string

---
+++ md-20-string/type-strilng.js
@@ -0,0 +1,9 @@
+const r = 'rayhan';
+const m = "mamun";
+const a = `reday`;
+const b = new String('riyad');
+
+console.log(typeof r);
+console.log(typeof m);
+console.log(typeof a);
+console.log(typeof b);
