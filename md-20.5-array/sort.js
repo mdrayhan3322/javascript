fb0b02393087b8ed38c1d856c78d23efb5834eb0
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-05 23:23:22 +0600

    differe t

---
+++ md-20.5-array/sort.js
@@ -0,0 +1,3 @@
+let ar = [3,2,6,4,6,3];
+let a = ar.sort();
+console.log(a);
---
+++ md-21-function/sum.js
@@ -0,0 +1,25 @@
+
+// function sum(a,b){
+//     const result = (a+b);
+//     return result;
+
+// }
+// const total = sum(7,6);
+// console.log(total);
+
+
+// -------------------------
+// function add(price1, price2){
+//     const a = price1+price2;
+//     return a;
+// }
+// const s = add(15,70);
+// console.log(s);
+// ---------------------------
+
+// function add2(a,b){
+//     return a+b;
+// }
+// const c = add2(15,70);
+// console.log(c);
+// ---------------------------
