d4a6e71e6518fb3f91ab9cf4ad0fddec076fcaf0
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-05 13:03:14 +0600

    recerse

---
+++ md-20.5-array/arra.js
@@ -0,0 +1,25 @@
+// const ar= ['rohim','korem','borkort']
+// for(const friend of ar){
+//     console.log(friend);
+// }
+// -------------------------
+// const ar= ['rohim','korem','borkort'];
+// for(let i=0; i<ar.length; i++){
+//     console.log(ar[i]);
+// }
+
+// ------------------------------
+
+// const ar = [10,3,4,3,5,3,54,3,53];
+// for(let i=0; i<ar.length; i++){
+//     console.log(ar[i]);
+// }
+
+// ---------------------------------
+
+const ar = [10,3,4,3,5,344,54,3,53];
+n=0;
+while(n < ar.length){
+    console.log(ar[n]);
+    n++;
+}
