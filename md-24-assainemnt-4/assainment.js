0e57d2b620df6541e44d01cbd2ca98242fc89805
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-09 12:48:28 +0600

    dudapt array

---
+++ md-24-assainemnt-4/assainment.js
@@ -0,0 +1,27 @@
+
+/**
+ 
+// question one
+
+ function cashOut( money ) {
+    if(money >0){
+        const cashOoutFee = money*1.75/100;
+        return cashOoutFee;
+
+    }
+    else if(money <0){
+        return  st = `"Invalid"`;
+    }
+    else{
+        if(typeof(money) === `string`){
+            return str = `"Incalid"`;
+        }
+    }
+    
+  
+}
+const cas = cashOut(5000);
+console.log(cas);
+// -------------------------------------------------------------
+ */
+
