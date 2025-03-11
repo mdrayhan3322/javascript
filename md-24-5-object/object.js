b3632ddbcbf049019c3a2fcd9732fb051082f3d7
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-11 16:08:12 +0600

    object

---
+++ md-24-5-object/object.js
@@ -0,0 +1,19 @@
+
+const objectName = {
+    name : 'Rayhan',
+    roll :68292,
+    department: 'computer',
+    grup: 'A',
+    friends: ['mahamud','arafat','epti','arif','dedar','asraful'],
+    arObject: [{name: 'masud', id:49394 } , {name:'naem',age:22}],
+    isSingle: true,
+    friObject : {
+        name :'Nijam',
+        roll : 30484,
+        student: 'good',
+
+    }
+
+}
+console.log(objectName);
+console.log(objectName.friObject);
