9476ec8c9bf9c2a442a89a6ef98aabad4b9b571a
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-04-23 10:55:06 +0600

    innerText

--- md-25-thml-add-in-Javahcript/video-1.js
+++ md-25-thml-add-in-Javahcript/video-1.js
@@ -1,4 +1,10 @@
 console.log("mara ka");
 console.log(document.body);
  const lilist = document.getElementsByTagName('li');
- console.log(lilist);
+//  console.log(lilist);
+for(const a of lilist){
+    console.log(a);
+}
+for(const a of lilist){
+    console.log(a.innerText);
+}
