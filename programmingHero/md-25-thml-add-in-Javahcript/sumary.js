953d62356e5638fec906d3030890c1c2982d9e1d
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-04-24 21:20:09 +0600

    sumai

---
+++ programmingHero/md-25-thml-add-in-Javahcript/sumary.js
@@ -0,0 +1,11 @@
+
+const li = document.getElementsByTagName("li");
+console.log(li);
+// class k kuje baher korar niom
+const classList = document.getElementsByClassName("section-header");
+console.log(classList);
+// id i kuje baher korar niom
+const fourSection = document.getElementById("sectionFour");
+console.log(fourSection);
+fourSection.style.color ="tomato";
+fourSection.style.backgroundColor="yellow";
--- sumitsaha/functionCode/sleep.js
+++ sumitsaha/functionCode/sleep.js
@@ -1,7 +1,24 @@
-function sleep(namee){
-    console.log(namee+"  sleping form 10 pm");
+// function sleep(namee){
+//     console.log(namee+"  sleping form 10 pm");
+// }
+// console.log(namee);
+// sleep("rayhan" );
+// sleep("mahamud");
+// sleep("kamal");
+
+// ----------------------------------------------
+// function timee(name, time){
+//     console.log(name+" sleeping from "+time);
+// }
+// timee("Rayhan","12PM");
+// timee("Mahamud","11PM");
+// timee("Arafat","4PM");
+
+// -----------------------------------------------
+
+function caurName(){
+    const caur = "value";
+    console.log(caur);
 }
-console.log(namee);
-sleep("rayhan" );
-sleep("mahamud");
-sleep("kamal");
+caurName();
+console.log(caur);
