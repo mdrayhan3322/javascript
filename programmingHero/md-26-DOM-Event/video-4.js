0e8ac07d073a565736a99ee8ee9fb7a0104ae3ed
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-04-26 18:00:10 +0600

    chinge text

---
+++ programmingHero/md-26-DOM-Event/video-4.js
@@ -0,0 +1,12 @@
+document.getElementById('chinge-title')
+.addEventListener('click', function (){
+    const pageTitle = document.getElementById('page-title');
+    pageTitle.innerText='chine title';
+    
+})
+// ------------------------------------------
+document.getElementById('logEnButton')
+.addEventListener('click',function(){
+    const loginPage = document.getElementById("title-ching-tag");
+    loginPage.innerText= 'user loggend in successfule';
+})
