bd228337d85d4b8454a0ff280c70e9922c34534f
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-04-26 16:31:26 +0600

    funtcion

--- programmingHero/md-26-DOM-Event/video-1.js
+++ programmingHero/md-26-DOM-Event/video-1.js
@@ -1,7 +1,22 @@
 
+// option one--------------
 function makeRed(){
     document.body.style.backgroundColor='red';
 }
 function makeYellow(){
     document.body.style.backgroundColor='yellow';
+}
+
+// option 2----------------------
+const buttonBlue = document.getElementById("btn-blue");
+buttonBlue.onclick = function buttonBlueCollor(){
+    document.body.style.backgroundColor = 'blue';
+}
+// -----------------------------------------
+// option 3 ------------------------
+const buttonPurpleColor = document.getElementById("btn-purple");
+
+buttonPurpleColor.onclick = blueFunction;
+function blueFunction(){
+    document.body.style.backgroundColor='purple';
 }
