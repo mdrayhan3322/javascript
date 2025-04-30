352dfb586a7d76ea6c9c2a9737b4a7c84e71c725
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-04-30 11:12:18 +0600

    add money section done

--- programmingHero/md-28-pay-mobail-bank/script/addmoney.js
+++ programmingHero/md-28-pay-mobail-bank/script/addmoney.js
@@ -9,3 +9,37 @@ document.getElementById('add-money-box').addEventListener('click',
     }
 )
 // cashout section
+document.getElementById('cashout-box').addEventListener('click',
+    function(){
+        document.getElementById('addmoney').style.display = 'none';
+        document.getElementById('cashout').style.display = 'block';
+    }
+)
+// input value chaken section
+document.getElementById('add-money').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        const accounNumber = document.getElementById('Acount-number').value;
+        const pinNumber = document.getElementById('pin').value;
+        const convertedPinNumber = parseInt(pinNumber);
+        const amountNumber = document.getElementById('amount').value;
+        const convertedAmountNumber = parseFloat(amountNumber); 
+        const mainBalance = document.getElementById('main-balance').innerText;
+        const convertedMainBalance = parseFloat( mainBalance);
+
+       
+
+        if(accounNumber.length === 11){
+            if(convertedPinNumber === 1234){
+                const sum = convertedMainBalance + convertedAmountNumber;
+                document.getElementById('main-balance').innerText = sum;
+
+               
+            }
+            else alert('tomar pin number tik nai');
+           
+        }
+        else console.log('tomar pin tik nai');
+      
+    }
+)
