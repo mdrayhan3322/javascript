3545fd8bd4afe757f4ce40082f596427252bf323
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-20 13:57:53 +0600

    object

---
+++ programmingHero/md-30-ES6/object.js
@@ -0,0 +1,10 @@
+const student ={
+    name : "nejam",
+    roll : 23,
+    frend: ['rohim','korem', 'daud','rayhan']
+    
+};
+// console.log(student);
+const a = student.frend;
+console.log(...a);
+// -------------------------------------
