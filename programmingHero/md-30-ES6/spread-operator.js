ef3cc22a29899e60483bcd21105f5da30e2dcbd2
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-20 13:48:22 +0600

    spread operator

---
+++ programmingHero/md-30-ES6/spread-operator.js
@@ -0,0 +1,11 @@
+
+// spread operator
+const a = ['rayhan','mamun','korem','rohman'];
+// console.log(...a);
+// --------------------------------
+
+const b = ['a','b','c'];
+const c = ['d',...b];
+// console.log(b);
+// console.log(...b);
+console.log(c);
