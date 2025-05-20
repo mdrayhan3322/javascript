1ef2b6f403714df7d34d33d6ae6de38ae5c8af7e
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-20 14:24:17 +0600

    array sturcting

--- programmingHero/md-30-ES6/object.js
+++ programmingHero/md-30-ES6/object.js
@@ -6,5 +6,27 @@ const student ={
 };
 // console.log(student);
 const a = student.frend;
-console.log(...a);
+// console.log(...a);
 // -------------------------------------
+
+const  car = {
+    name : 'bmw',
+    price:230,
+    model:['a','b','c']
+
+};
+// --------------------------------------------
+// structure const {}=objectName;
+const {name,price,model}=car;
+// console.log(name);
+// console.log(price);
+
+// 
+
+// --------------------------------
+// array sturcturing structure
+//dataType variable = [] arrayName;
+
+const ar = ['rayhan','mahamud',{obj:'and'},'arafat'];
+const [ab,c]=ar;
+console.log(ab);
