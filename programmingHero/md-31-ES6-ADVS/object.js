1118abaefd4a44af6404df4c41629bc31f79b53c
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-20 23:55:13 +0600

    ternare operator

---
+++ programmingHero/md-31-ES6-ADVS/object.js
@@ -0,0 +1,14 @@
+
+
+const person = {
+    name:'rayhan',
+    age:91,
+    father:{
+        name:'jashim uddin',
+        age:54,
+
+    }
+}
+console.log(person.father?.frend);
+console.log(person?.mother);
+
