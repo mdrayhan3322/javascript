db7fefe201e5f90c552c564540af59d825216f28
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-20 21:18:07 +0600

    object function

--- programmingHero/md-30-ES6/arra.js
+++ programmingHero/md-30-ES6/arra.js
@@ -2,4 +2,14 @@
 const ar = ['rayhan','mamun','riday','rokon'];
 // console.log(ar);
 // console.log(...ar);
-console.log(`amar sonar bangla ami tomai balo base ${ar[2]}`)
+// console.log(`amar sonar bangla ami tomai balo base ${ar[2]}`)
+// --------------------------------------------------------------------
+
+const a = {
+    name : 'rayhan',
+    age: 33,
+
+}
+console.log(Object.keys(a));
+console.log(Object.values(a));
+
