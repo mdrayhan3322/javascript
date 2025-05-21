4d4566fa9c6ed717d525a9645249d9c55fc176fc
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-21 20:21:27 +0600

    prodect class

--- programmingHero/md-31-ES6-ADVS/class.js
+++ programmingHero/md-31-ES6-ADVS/class.js
@@ -1,10 +1,26 @@
-class product {
-     a = "bangladesh";
-      constructor(name,price,color){
-      console.log(name,price,color);
+// class product {
+//      a = "bangladesh";
+//       constructor(name,price,color){
+//       console.log(name,price,color);
 
-     }
+//      }
 
+// }
+
+// ----------------------------------------
+class product{
+    constructor(name,price,brand){
+        this.name=name;
+        this.price=price;
+        this.border= brand;
+    }
+    detels(){
+        console.log('ami detels',this);
+    }
 }
 
-const iphone =  new product('iphone','20','red');
+const sumsong = new product('Iphone','50','somsong');
+console.log(sumsong);
+console.log(sumsong.name);
+
+ sumsong.detels()
