b415ca078ca696b52094a7ccc60555d2be37a7db
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-22 22:18:06 +0600

    closure functon

---
+++ programmingHero/md-32-ES6/function.js
@@ -0,0 +1,16 @@
+
+function sum (){
+    let counter = 0;
+    const newfunctio = function (){
+        counter++;
+        console.log(counter);
+
+    }
+    newfunctio();
+    newfunctio();
+    // newfunctio();
+
+
+}
+sum();
+sum();
