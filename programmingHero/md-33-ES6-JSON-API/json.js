a4a3d2575b75bf59be3bf21f0042d007abe6b0fa
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-24 12:27:56 +0600

    json

---
+++ programmingHero/md-33-ES6-JSON-API/json.js
@@ -0,0 +1,16 @@
+
+
+const person = {
+    name : "rayhan",
+    age  : 32,
+    friend: ['jobair','masud','jahid','nijam','samsuddin']
+};
+console.log(typeof person); /* person holo akta object */
+
+const convertedPerson = JSON.stringify(person); /* object k JSON  a converted kora holo */
+console.log(convertedPerson);
+
+/* abat JSON the k object a converted kora holo nice */
+const convertedObject =  JSON.parse(convertedPerson);
+console.log(convertedObject);
+
