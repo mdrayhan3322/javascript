ff9f09d113755f1e5beda5536c82919950f71455
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-28 23:32:09 +0600

    coll stact

---
+++ programmingHero/md-34-ES6/coll-stack.js
@@ -0,0 +1,24 @@
+
+function first(){
+    secend();
+    console.log('mama ami first function');
+
+}
+function secend(){
+    therd();
+    console.log('mama ami secen function')
+
+}
+function therd(){
+    four()
+    console.log('mama ami three function')
+
+
+
+}
+function four(){
+    console.log('mama ami four function');
+
+}
+
+first();
