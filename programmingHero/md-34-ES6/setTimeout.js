c226074515777ea94e79f5662d207981e1ab42db
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-05-31 11:27:18 +0600

    setTimeOut function and syncronyes and asyncronyes

---
+++ programmingHero/md-34-ES6/setTimeout.js
@@ -0,0 +1,35 @@
+
+function person1(){
+    console.log('ami person one ');
+
+}
+function person2(){
+    console.log('ami person tow ');
+
+
+}
+function person3(){
+    setTimeout( ()=>{
+        console.log("ame person three ami 10 secend pore run hove");
+        
+    } ,10000);
+
+}
+function person4(){
+    console.log('ami person four');
+
+}
+ function person5(){
+    setTimeout(()=>{
+        console.log('ami person  five ami 2 secend por print hovo');
+        
+    },2000);
+
+ }
+ person1();
+ person2();
+ person3();
+ person4();
+ person5();
+
+ 
