384ea686ff73021ae88d9fb34e6aab79a88464dd
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-05 09:44:34 +0600

    pormies

--- programmingHero/md-34-ES6/respons.js
+++ programmingHero/md-34-ES6/respons.js
@@ -1,13 +1,20 @@
 
+// respos
+// const respons = ()=>{
+//     // console.log("mama mai akane");
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(respons =>console.log(respons))
 
+// }
+// respons();
+
+// ---------------------------------------------
+// promis-----
 const respons = ()=>{
     // console.log("mama mai akane");
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(respons =>console.log(respons))
+    .then(respons =>console.log(respons.json()))
 
 }
 respons();
-
-// ---------------------------------------------
-
 
