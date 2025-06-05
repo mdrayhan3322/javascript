09888b72ae262fe2f0a65079ab62fb05afb6fbde
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-05 09:40:52 +0600

    promies

---
+++ programmingHero/md-34-ES6/respons.js
@@ -0,0 +1,13 @@
+
+
+const respons = ()=>{
+    // console.log("mama mai akane");
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(respons =>console.log(respons))
+
+}
+respons();
+
+// ---------------------------------------------
+
+
